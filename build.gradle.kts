70b9152a76ad5adc171777b7bb14d30d06bacc47
András Bögöly <andras.bogoly.91@gmail.com>
2022-02-01 20:58:03 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    java
+    id("org.springframework.boot") version "2.6.3"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+}
+
+group = "com.bogolyandras"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/java/com/bogolyandras/demoapp/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.bogolyandras.demoapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/helloworld")
+    public String yoo() {
+        return "asfasf";
+    }
+
+}
